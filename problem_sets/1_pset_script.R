04a09f1a0f027a97ff0b27a4a63a4f0dcccbf69b
Andie Creel <andie.creel@gmail.com>
2024-01-10 14:11:34 -0500

    pset script

---
+++ problem_sets/1_pset_script.R
@@ -0,0 +1,22 @@
+# YOUR NAME / DATE
+# GOAL 
+
+# Problem 1: Basic Operations
+# Insert Code here 
+
+
+# Problem 2: Working with Vectors
+# Insert code here 
+
+# Problem 3: Basic Data Frame Manipulation
+# Insert code here 
+
+# Problem 4: Loop and Function
+#Insert code here 
+
+# Problem 5: Conditional Logic
+# Insert code here 
+
+# Problem 6: Reflect
+# Write your reflection as a comment here
+
